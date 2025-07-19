c012728db16057802eae872151bf301e168b97b8
Joe Hentges <joeypeterhentges@gmail.com>
2025-07-19 14:56:40 -0500

    delete health service files

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,13 @@
 import express from "express"
-import { pino } from "pino"
 
 import { isSecure } from "@/middleware/is-secure"
 
 import { createCatService, getCatService } from "@/services/cats"
-import { getHealthService } from "@/services/health"
 
-const logger = pino({ name: "routes" })
 const router = express.Router()
 
 // Health
-router.get("/health", getHealthService)
+router.get("/health", (_, res) => res.send("Server is running!"))
 
 // Cats
 router.get("/cats/:catId", getCatService)
--- src/services/health/get-health-service.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express"
-
-export function getHealthService(_: Request, res: Response) {
-  res.send("Server is running!")
-}
